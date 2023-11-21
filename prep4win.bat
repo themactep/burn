366bce6260906ecfb409e099bef94ed7130fba4c
Paul Philippov <themactep@gmail.com>
2023-11-21 01:57:56 -0500

    replace symlinks with copies of files on windows

---
+++ prep4win.bat
@@ -0,0 +1,15 @@
+del gk7202v300.json gk7205v200.json gk7205v300.json ^
+  hi3516dv300.json hi3516ev100.json hi3516ev300.json ^
+  hi3518ev200.json hi3518ev201.json hi3518ev300.json ^
+  hi3520dv100.json
+copy hi3516cv200.json hi3518ev200.json
+copy hi3516cv200.json hi3518ev201.json
+copy hi3516cv300.json hi3516ev100.json
+copy hi3516cv500.json hi3516dv300.json
+copy hi3516ev200.json gk7202v300.json
+copy hi3516ev200.json gk7205v200.json
+copy hi3516ev200.json hi3516ev300.json
+copy hi3516ev200.json hi3518ev300.json
+copy hi3516ev300.json gk7205v300.json
+copy hi3520dv200.json hi3520dv100.json
+
